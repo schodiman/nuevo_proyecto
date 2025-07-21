62772d38178517b6b3b63d93354d29a368f23f94
schodiman <schodiman.h@gmail.com>
2025-07-21 18:43:58 -0400

    Add files via upload

---
+++ auxiliares/opciones_menu.py
@@ -0,0 +1,15 @@
+menu_principal = {
+    1: 'Gestión Asignaturas',
+    2: 'Gestión Docentes',
+    3: 'Gestión Estudiantes',
+    4: 'Gestión Notas',
+    0: 'Salir'
+}
+
+menu_asignaturas = {
+    1: 'Listado Asignaturas',
+    2: 'Agregar Asignatura',
+    3: 'Modificar Asignatura',
+    4: 'Eliminar Asignatura',
+    0: 'Volver al Menú Anterior'
+}
